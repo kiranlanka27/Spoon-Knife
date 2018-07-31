7dde143bb42a5e815e11eedad24990ea6f2c5a11
Kiran Kumar Lanka <kiran.lanka27@gmail.com>
2018-07-31 11:09:38 -0400

    Added Calc java program

---
+++ Calcu.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Calcu {
+
+	public static int mult(int a, int b)
+	{
+		int c ;
+		c=a*b;
+		return c;
+	}
+	
+	public static int add(int a, int b)
+	{
+		int c ;
+		c=a+b;
+		return c;
+	}
+	
+	public static int sub(int a, int b)
+	{
+		int c ;
+		c=a-b;
+		return c;
+	}
+	
+	public static int div(int a, int b)
+	{
+		int c ;
+		c=a/b;
+		return c;
+	}
+}
